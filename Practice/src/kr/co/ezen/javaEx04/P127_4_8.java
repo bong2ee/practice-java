505930fb89c1213cae5b24d7ebb9367650686546
bong2ee <qhduddlfkda@gmail.com>
2023-03-05 22:46:07 +0900

    자바의 정석 예제

---
+++ Practice/src/kr/co/ezen/javaEx04/P127_4_8.java
@@ -0,0 +1,15 @@
+package kr.co.ezen.javaEx04;
+/*
+ * Math.random()을 이용해
+ * 1~6 사이의 임의의 정수를 변수 value에 저장하는 코드
+ */
+public class P127_4_8 {
+
+	public static void main(String[] args) {
+		int value = (int)(Math.random() * 6) + 1;
+		
+		System.out.println("value:"+value);
+
+	}
+
+}
