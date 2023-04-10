cea2db18284e3d34616aefa4ebe240f5357360d4
bong2ee <qhduddlfkda@gmail.com>
2023-04-10 21:23:07 +0900

    명품 자바 프로그래밍 예제

---
+++ Practice/src/kr/co/ezen/mjp/mjp03/h/H0310.java
@@ -0,0 +1,19 @@
+package kr.co.ezen.mjp.mjp03.h;
+
+public class H0310 {
+
+	public static void main(String[] args) {
+		
+		int[][] arr = new int[4][4];
+		
+		for(int i=0; i<arr.length; i++) {
+			for(int j=0; j<arr[i].length; j++) {
+					arr[i][j] = (int)(Math.random()*10+1);
+				System.out.print(arr[i][j] + "  ");
+			}
+			System.out.println();	
+		}
+		
+	}
+}
+
