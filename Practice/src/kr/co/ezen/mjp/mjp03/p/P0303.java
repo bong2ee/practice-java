cd12709e90c0a355fb172f1926d1519f6baf22d8
bong2ee <qhduddlfkda@gmail.com>
2023-04-20 21:39:28 +0900

    명품 자바 프로그래밍 예제

---
+++ Practice/src/kr/co/ezen/mjp/mjp03/p/P0303.java
@@ -0,0 +1,15 @@
+package kr.co.ezen.mjp.mjp03.p;
+
+public class P0303 {
+	
+	public static void main(String[] args) {
+		
+		char ch = 'a';
+		do {
+			System.out.print(ch++ + " ");
+		} while (ch <= 'z');
+			
+			
+	}
+
+}
